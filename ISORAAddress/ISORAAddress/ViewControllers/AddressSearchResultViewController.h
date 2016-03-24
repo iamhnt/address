aad3eaf81518602ada544f12aa3b2e6ed4e84dd4
Ho Ngoc Thang <iamhnt@gmail.com>
2016-03-25 02:38:40 +0700

    commit merge

---
+++ ISORAAddress/ISORAAddress/ViewControllers/AddressSearchResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdressSearchResultViewController.h
+//  ISORAAddress
+//
+//  Created by Ho Ngoc Thang on 3/25/16.
+//  Copyright © 2016 Ho Ngoc Thang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddressSearchResultViewController : UIViewController
+
+@end
---
+++ ISORAAddress/ISORAAddress/ViewControllers/AddressSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddressSearchViewController.h
+//  ISORAAddress
+//
+//  Created by Ho Ngoc Thang on 3/25/16.
+//  Copyright © 2016 Ho Ngoc Thang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddressSearchViewController : UIViewController
+
+@end
