da3823cb64f4304813d44bbfeae4e11b23a77417
Ho Ngoc Thang <iamhnt@gmail.com>
2016-03-25 00:20:37 +0700

    merge commit

---
+++ ISORAAddress/ISORAAddress/Helper/Helper.h
@@ -0,0 +1,14 @@
+//
+//  Helper.h
+//  ISORAAddress
+//
+//  Created by Ho Ngoc Thang on 3/25/16.
+//  Copyright © 2016 Ho Ngoc Thang. All rights reserved.
+//
+
+#ifndef Helper_h
+#define Helper_h
+
+#define NOTAVAILABLE @"This function is under construction. Which will be available shortly."
+
+#endif /* Helper_h */
